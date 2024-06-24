fad89862c68faaa1b19f580a2302f67263fd11d1
malcolm-a <aridoryjr@icloud.com>
2024-06-24 22:05:54 +0200

    Added a function to determine who beats who

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,14 @@ use rand::seq::IndexedRandom;
 use std::io;
 use std::vec;
 
+pub fn beats(player: &str, foe: &str) -> bool {
+    if player == "g" && foe == "w" {
+        true
+    } else if player == "w" && foe == "f" {
+        true
+    } else if player == "f" && foe == "g" {
+        true
+    } else {
+        false
+    }
+}
